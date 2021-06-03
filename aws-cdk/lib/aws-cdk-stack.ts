594a741beb98b60da29f2742eb3a8098be76c1ae
Mirco Kraenz <kraenzmirco@gmail.com>
2021-06-03 22:14:46 +0200

    aws cdk: allow destroy

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,12 +1,15 @@
 import { Bucket } from "@aws-cdk/aws-s3";
 import * as cdk from "@aws-cdk/core";
+import { RemovalPolicy } from "@aws-cdk/core";
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     new Bucket(this, "awscdkbucket", {
       versioned: true,
-      bucketName: "my-bucket-name",
+      bucketName: "my-bucket-name-from-cdk",
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
     // The code that defines your stack goes here
