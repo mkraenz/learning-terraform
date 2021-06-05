5ce601d28545078d54d520bacf2181e604c1102b
Mirco Kraenz <kraenzmirco@gmail.com>
2021-06-05 20:29:49 +0200

    ADD aws CDK workshop lambda + apigateway
    https://cdkworkshop.com/20-typescript/30-hello-cdk.html

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,17 +1,19 @@
-import { Bucket } from "@aws-cdk/aws-s3";
+import * as gateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
 import * as cdk from "@aws-cdk/core";
-import { RemovalPolicy } from "@aws-cdk/core";
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new Bucket(this, "awscdkbucket", {
-      versioned: true,
-      bucketName: "my-bucket-name-from-cdk",
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
 
     // The code that defines your stack goes here
+    const helloLambda = new lambda.Function(this, "hello-lambda", {
+      code: lambda.Code.fromAsset("lambda"),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "hello.handler", // hello.js = filename, handler = exported function
+    });
+    new gateway.LambdaRestApi(this, "endpoint", {
+      handler: helloLambda,
+    });
   }
 }
